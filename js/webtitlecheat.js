a845fd9c7565459c8dec74557cc3cedd65374f00
xiaobencong <sunyiqiang17@gmail.com>
2020-05-18 15:26:38 +0800

    Site updated: 2020-05-18 15:26:28

--- js/webtitlecheat.js
+++ js/webtitlecheat.js
@@ -1,15 +1,15 @@
-// �������Ц����
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '�q(��A��`)�r ҳ������� ~';
+        $('[rel="icon"]').attr('href', "/images/bad1.png");
+        document.title = '╭(°A°`)╮ 页面崩溃啦~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(?>��<*?) ���ֺ��� ~' + OriginTitle;
+        $('[rel="icon"]').attr('href', "/images/good1.png");
+        document.title = '٩(๑❛ᴗ❛๑)۶ 噫又好啦~';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
